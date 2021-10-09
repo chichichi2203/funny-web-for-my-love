56821c9bbd573b08c7a5772ef85e24e1fb4c4961
dinkha-phan <73351060+dinkha-phan@users.noreply.github.com>
2021-10-09 16:53:44 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+import anime from "https://cdn.skypack.dev/animejs@3.2.1";
+
+anime({
+    targets: '.birthday-cake svg path',
+    strokeDashoffset: [anime.setDashoffset, 0],
+    easing: 'easeInOutSine',
+    duration: 5000,
+    delay: function (el, i) { return i * 1000; },
+    direction: 'forwards',
+    loop: true,
+});
+
+
